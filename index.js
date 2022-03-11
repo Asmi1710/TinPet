a377cfbf10697de9d4f7ce29e5684fdf48b48134
asmita <asmita.singh1710@gmil.com>
2022-03-11 10:08:53 +0530

    adding effects1

--- index.js
+++ index.js
@@ -18,4 +18,34 @@ for (var j=0; j<k;j++) {
 }
 
 
+visualViewport.addEventListener("scroll", changeTitle);
+
+ function changeTitle (){
+     const text =document.querySelectorAll(".feature-title")[0];
+     const strText =text.textContent;
+     const splitText= strText.split ("");
+    
+     text.textContent="";
+     for (var i=0;i<splitText.length;i++){
+          text.innerHTML+="<span>"+ splitText[i]+"</span>";
+      }
+
+     let char=0;
+     let timer = setInterval (flicker,50);
+
+     function flicker () {
+         const span =text.querySelectorAll ("span")[char];
+         span.classList.add ("flick");
+         char++;
+         if (char==splitText.length){
+            complete ();
+            return;
+         }
+      }
+    function complete (){
+        clearInterval (timer);
+        timer=null;
+        console.log ("done");
+    }  
+ }
 
