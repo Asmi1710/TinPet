ec9dfac623be0408d9fcad724005131af4c8ccfb
asmita <asmita.singh1710@gmil.com>
2022-03-13 02:08:13 +0530

    changes to the gif in the feature section

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ for (var i=0;i<n;i++){
     });
 }
 
-
 // Opening new page when sign up button is clicked
 var plan = document.querySelectorAll(".pricingTable-signup");
 var k= plan.length;
@@ -19,9 +18,10 @@ for (var j=0; j<k;j++) {
     });
 }
 
+//****************************************************************************** */
 // adding animation to the title of tinpet
-document.querySelector(".navbar-brand").textContent=" tinpet";
-const titleHead = document.querySelector(".navbar-brand");
+document.querySelectorAll(".navbar-brand")[1].textContent=" tinpet";
+const titleHead = document.querySelectorAll(".navbar-brand")[1];
 const splitHead = titleHead.textContent.split("");
 titleHead.textContent="";
 console.log (splitHead);
@@ -99,7 +99,7 @@ function repeatFlick (k){
         span.classList.remove("flick");
     }
     let char=0;
-    let timer= setInterval (flicker,50);
+    let timer= setInterval (flicker,80);
     //console.log ("st1-again");
 
     function flicker () {
